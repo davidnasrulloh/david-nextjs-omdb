d1277d0d7cea2ad06c5b0f27803b8cae2dcbc5ca
David Nasrulloh <davidkrb52@gmail.com>
2024-09-11 09:42:36 +0700

    fix: import providers in layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import ProviderQueryClient from "@/hooks/ProviderQueryClient";
+import ProviderQueryClient from "@/providers/ProviderQueryClient";
 
 export const metadata: Metadata = {
     title: "Olympic - David",
